9a111a7be8ed95c6d448d8a750c6393e5e3fdfca
myj1217 <myj1217@naver.com>
2024-01-15 10:39:38 +0900

    update basketitem

--- front/src/components/BasketItem.js
+++ front/src/components/BasketItem.js
@@ -14,7 +14,7 @@ const BasketItemBlock = styled.div`
     padding-top: 2rem;
     padding-bottom: 2rem;
     .chad {
-      padding: 0 20px;
+      padding: 0 21px;
       cursor: pointer;
       display: flex;
       align-items: center;
